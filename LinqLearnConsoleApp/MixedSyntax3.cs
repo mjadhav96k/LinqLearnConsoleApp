df10c37d8a994cc601d8d439bbe48bf1d5105fb6
mayur <mjm090972@gmail.com>
2025-01-05 16:14:12 +0530

    Add project files.

---
+++ LinqLearnConsoleApp/MixedSyntax3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQDemo
+{
+    class MixedSyntax3
+    {
+        static void Main(string[] args)
+        {
+            //Data Source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            //LINQ Query using Mixed Syntax
+            var MethodSyntax = (from obj in integerList
+                                where obj > 5
+                                select obj).Sum();
+            //Execution
+            Console.Write("Sum Is : " + MethodSyntax);
+
+            Console.ReadKey();
+        }
+    }
+}
